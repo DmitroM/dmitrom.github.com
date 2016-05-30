4f55c3b88cbfabd43d63b5d2fafa0c881ebd2633
DmitroM <meldm007@gmail.com>
2016-05-31 00:47:42 +0400

    JQuery&AJAX Task v0.6

--- universityTasks/JS/TaskJQuery-AJAX/server.js
+++ universityTasks/JS/TaskJQuery-AJAX/server.js
@@ -1,9 +1,9 @@
-var WebSocketServer = require('ws').Server,
-	wss = new WebSocketServer({port: 8181});
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server('.');
 
-wss.on('connection', function(ws) {
-	console.log('client connected');
-	ws.on('message', function(message) {
-		console.log(message);
-	});
-});
+http.createServer(function(req, res) {
+	file.serve(req, res);
+}).listen(8080);
+
+console.log('Server running on port 8080');
